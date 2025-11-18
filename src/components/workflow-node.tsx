baf83b5c87c15798132c25cc5a0e5fdacb32e9a8
M.Talha <iamtalha971@gmail.com>
2025-11-18 23:27:11 +0400

    feat -> react flow editor initialized

---
+++ src/components/workflow-node.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { SettingsIcon, TrashIcon } from "lucide-react";
+import type { ReactNode } from "react";
+import { Button } from "./ui/button";
+
+interface WorkflowNodeProps {
+  children: ReactNode;
+  showToolbar?: boolean;
+  onDelete?: () => void;
+  onSettings?: () => void;
+  name?: string;
+  description?: string;
+}
+
+import React from "react";
+import { NodeToolbar, Position } from "@xyflow/react";
+
+const WorkflowNode = ({
+  children,
+  onDelete,
+  onSettings,
+  description,
+  name,
+  showToolbar = true,
+}: WorkflowNodeProps) => {
+  return (
+    <>
+      {showToolbar && (
+        <NodeToolbar>
+          <Button size={"sm"} variant={"ghost"} onClick={onSettings}>
+            <SettingsIcon className="size-4" />
+          </Button>
+          <Button size={"sm"} variant={"ghost"} onClick={onDelete}>
+            <TrashIcon className="size-4" />
+          </Button>
+        </NodeToolbar>
+      )}
+      {children}
+      {name && (
+        <NodeToolbar
+          position={Position.Bottom}
+          isVisible
+          className="w-max-[200px] text-center"
+        >
+          <p className="font-medium">{name}</p>
+          {description && (
+            <div className="text-muted-foreground truncate text-sm">
+              {description}
+            </div>
+          )}
+        </NodeToolbar>
+      )}
+    </>
+  );
+};
+
+export default WorkflowNode;
