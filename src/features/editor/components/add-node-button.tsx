22897eb658da4ecfa6b1e8cf94df45dfcd0fabe7
M.Talha <iamtalha971@gmail.com>
2025-11-19 23:42:25 +0400

    upgrade -> node type schema in prisma

--- src/features/editor/components/add-node-button.tsx
+++ src/features/editor/components/add-node-button.tsx
@@ -1,18 +1,23 @@
 "use client";
 
+import { NodeSelector } from "@/components/node-selector";
 import { Button } from "@/components/ui/button";
 import { PlusIcon } from "lucide-react";
+import { useState } from "react";
 
 export const AddNodeButton = () => {
+  const [selectorOpen, setSelectorOpen] = useState(false);
   return (
-    <Button
-      size={"icon"}
-      variant={"outline"}
-      className="bg-background"
-      onClick={() => {}}
-    >
-      <PlusIcon />
-    </Button>
+    <NodeSelector open={selectorOpen} onOpenChange={setSelectorOpen}>
+      <Button
+        size={"icon"}
+        variant={"outline"}
+        className="bg-background"
+        onClick={() => {}}
+      >
+        <PlusIcon />
+      </Button>
+    </NodeSelector>
   );
 };
 
