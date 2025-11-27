1966b62182afc2f8d173055447ae02b8e9d247ee
M.Talha <iamtalha971@gmail.com>
2025-11-28 00:10:17 +0400

    update -> http executor data

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -1,14 +1,23 @@
 import { NodeExecutor } from "@/features/executions/types";
+import { NonRetriableError } from "inngest";
 
-type HttpRequestData = Record<string, unknown>;
+type HttpRequestData = {
+  body?:string,
+  endpoint?:string
+  method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+};
 
 export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
+  data,
   nodeId,
   context,
   step,
 }) => {
   // TODO: Publish "loading" state for http request
 
+  if(!data.endpoint){
+    throw new NonRetriableError("HTTP request node: Endpoint is missing")
+  }
   const result = await step.run("http-request", async () => context);
 
   // TODO: Publish "Success" state for http request
