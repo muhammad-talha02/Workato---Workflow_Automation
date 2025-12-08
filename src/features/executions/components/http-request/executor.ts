58e8d709c697ac36521e851ec0ca1f657ac727d3
M.Talha <iamtalha971@gmail.com>
2025-12-08 23:44:56 +0400

    feat -> add ky for http node execution

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -1,9 +1,9 @@
 import { NodeExecutor } from "@/features/executions/types";
 import { NonRetriableError } from "inngest";
-
+import ky, { KyRequest, Options } from "ky";
 type HttpRequestData = {
-  body?:string,
-  endpoint?:string
+  body?: string;
+  endpoint?: string;
   method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
 };
 
@@ -15,11 +15,36 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
 }) => {
   // TODO: Publish "loading" state for http request
 
-  if(!data.endpoint){
-    throw new NonRetriableError("HTTP request node: Endpoint is missing")
+  if (!data.endpoint) {
+    throw new NonRetriableError("HTTP request node: Endpoint is missing");
   }
-  const result = await step.run("http-request", async () => context);
+  const result = await step.run("http-request", async () => {
+    const endpoint = data.endpoint!;
+    const method = data.method || "GET";
+
+    const options: Options = { method };
+
+    if (["PUT", "PATCH", "POST"].includes(method)) {
+      if (data.body) {
+        options.body = data.body;
+      }
+    }
+    const response = await ky(endpoint, options);
+    const contentType = response.headers.get("content-type");
+    const responseData = contentType?.includes("application/json")
+      ? await response.json().catch(() => response.text())
+      : await response.text();
+    return {
+      ...context,
+      httpResponse: {
+        status: response.status,
+        statusText: response.statusText,
+        data: responseData,
+      },
+    };
+  });
 
   // TODO: Publish "Success" state for http request
+
   return result;
 };
