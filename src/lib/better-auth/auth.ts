8680313f8a5e9acfd2a3fa261b46fd07bbba7f36
M.Talha <iamtalha971@gmail.com>
2026-01-04 21:32:16 +0400

    added -> trusted origin

--- src/lib/better-auth/auth.ts
+++ src/lib/better-auth/auth.ts
@@ -9,6 +9,7 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  trustedOrigins:['https://workato.vercel.app'],
   emailAndPassword: {
     enabled: true,
     autoSignIn: true,
